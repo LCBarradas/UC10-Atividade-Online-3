187a7317f23cda24a7f2ea8b902de6afef577960
Luiz Carlos Barradas <100002313+LCBarradas@users.noreply.github.com>
2022-08-13 00:18:46 -0300

    Add files via upload

---
+++ ATIVI_ON3.sql
@@ -0,0 +1,15 @@
+
+
+INSERT INTO Usuarios VALUES ('Aphonso', 'Senha1234');
+INSERT INTO Classes VALUES ('Barbaro', 'Descrição do Barbaro');
+INSERT INTO Habilidades VALUES ('Lança Mortal'),('Escudo Supremo');
+INSERT INTO Personagens VALUES ('Deu Bug',1,1);
+INSERT INTO classesHabilidades VALUES (1,1), (1,2);
+
+INSERT INTO Usuarios VALUES ('Danox', 'Senha1234');
+INSERT INTO Classes VALUES ('Monge', 'Descrição do Monge');
+INSERT INTO Habilidades VALUES ('Recuperar a vida');
+INSERT INTO Personagens VALUES ('BitBug', 2,2);
+INSERT INTO classesHabilidades VALUES (2,2), (2,3);
+
+SELECT * FROM Usuarios;
